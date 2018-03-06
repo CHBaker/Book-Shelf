43f841447b4343e029bbc3b101d268dce58a0e97
CHBaker <charlesprobaker@mail.com>
2018-03-05 20:04:47 -0700

    FEATURE: navigation, page skeletons
    - Nav links and urls
    - Books specific to each page

--- src/App.js
+++ src/App.js
@@ -6,43 +6,22 @@ import Nav from './nav/Nav'
 import Reads from './reads/Reads'
 
 class App extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.handler = this.handler.bind(this)
-    // }
-
-    // handler(e) {
-    //     e.preventDefault()
-    //     this.setState({
-    //         someVar: someValue
-    //     })
-    // }
-
-    state = {
-        books: []
-    }
-
-    componentDidMount() {
-        this.getBooks()
-    }
-
-    getBooks () {
-        booksApi.getAll().then((books) => {
-            const current = books.filter((book) => book.shelf === 'currentlyReading')
-            this.setState({ books: current })
-            console.log(this.state.books)
-        })
-    }
 
     render() {
         return (
             <div>
                 <Nav />
                 <Route exact path='/' render={() => (
-                    <Reads books={this.state.books} />
+                    <Reads page={'currentlyReading'} />
                 )} />
                 <Route exact path='/toread' render={() => (
-                    <Reads books={this.state.books} />
+                    <Reads page={'wantToRead'} />
+                )} />
+                <Route exact path='/read' render={() => (
+                    <Reads page={'read'} />
+                )} />
+                <Route exact path='/search' render={() => (
+                    <h1>search shit</h1>
                 )} />
             </div>
         )
--- src/reads/Reads.js
+++ src/reads/Reads.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react'
+import * as booksApi from '../utils/BooksApi'
 
 class Reads extends Component {
-    render (props) {
+
+    state = {
+        books: [],
+        title: '',
+        page: this.props.page
+    }
+
+    componentDidMount () {
+        this.updatePage()
+        this.getBooks()
+    }
+
+    getBooks () {
+        booksApi.getAll().then((books) => {
+            console.log(this.props)
+            const current = books.filter((book) => book.shelf === this.props.page)
+            this.setState({ books: current })
+            console.log(this.state.books)
+        })
+    }
+
+    updatePage() {
+        switch (this.props.page) {
+            case 'currentlyReading':
+                this.setState({title: 'Currently Reading'})
+                break;
+            case 'wantToRead':
+                this.setState({title: 'Want to Read'})
+                break;
+            case 'read':
+                this.setState({title: 'Read'})
+                break;
+            default:
+                this.setState({title: 'Currently Reading'})
+        };
+    }
+
+    addToShelf(bookId, shelf) {
+        this.booksApi.shelf(bookId, shelf);
+    }
+
+    render(props) {
         return (
-            <ul>
-                {this.props.books.map((book) => (
-                    <li key={book.id}>{book.title}</li>
-                ))}
-            </ul>
+            <div>
+                <div className='title'>
+                    <h1>{this.state.title}</h1>
+                </div>
+                <ul>
+                    {this.state.books.map((book) => (
+                        <li key={book.id}>{book.title}</li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
